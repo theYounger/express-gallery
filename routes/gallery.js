bca5d2a1e36442c3b42546f60432fc820cfe4675
theYounger <kuyehara1@gmail.com>
2016-07-22 09:04:21 -1000

    edit to gallery route

--- routes/gallery.js
+++ routes/gallery.js
@@ -8,26 +8,46 @@ Router.use(bodyParser.json());
 Router.use(bodyParser.urlencoded({ extended: true }));
 
 Router.route('/')
+/* to view a list of gallery photos */
   .get( ( req, res ) => {
-    res.send('hi world');
+    res.send('hi gallery get');
   })
-  .get( '/:id', (req, res) => {
-    res.send('hi world 2');
-  })
-  .get( '/new', ( req, res ) => {
-
-  })
+/* to create a new gallery photo */
   .post( ( req, res ) => {
-
-  })
-  .get( '/:id/edit', ( req, res ) => {
-
-  })
-  .put( '/:id', ( req, res ) => {
-
-  })
-  .delete ( '/:id', ( req, res ) => {
-
+    res.send('hi gallery post');
   });
+
+Router.route('/:id')
+/*  to see a single gallery photo */
+  .get( (req, res) => {
+    res.send('hi gallery/:id get');
+  })
+/*  updates a single gallery photo identified by the :id param */
+  .put( ( req, res ) => {
+    res.send('hi gallery/:id put');
+  })
+/* to delete a single gallery photo identified by the :id param */
+  .delete ( ( req, res ) => {
+    db.post.destroy({
+      where : {
+        id : req.params.id
+      }
+    })
+    .then(function(){
+      // then redirect to gallery
+      res.redirect('/gallery');
+    });
+    // res.send('hi gallery/:id delete');
+  });
+
+/*  to see a "new photo" form */
+Router.get( '/new', ( req, res ) => {
+  res.send('hi gallery/new get');
+});
+
+/*  to see a form to edit a gallery photo identified by the :id param */
+Router.get( '/:id/edit', ( req, res ) => {
+  res.send('hi gallery/:id/edit get');
+});
 
 module.exports = Router;
