78e91b27d35420e32e81cc7affdbf375811048d7
theYounger <kuyehara1@gmail.com>
2016-07-26 10:18:37 -1000

    implement sequelize modeling to user auth example, authentication procedures now search sql database

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const session = require('express-session');
-const CONFIG = require('./config.json');
 const db = require('./models');
-
 const gallery = require('./routes/gallery');
 const authUser = require('./lib/middleware');
 
@@ -25,39 +23,40 @@ app.use(bodyParser.json());
 =        CONFIG EXAMPLE RETOOLING             =
 =============================================*/
 
-app.use(session({ secret: CONFIG.SECRET }));
+app.use(session({ secret: 'cat' }));
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/gallery', gallery);
 
+
 passport.use(new LocalStrategy(
   (username,password,done) => {
-    const CREDENTIALS = CONFIG.CREDENTIALS;
-    const USERNAME = CREDENTIALS.USERNAME;
-    const PASSWORD = CREDENTIALS.PASSWORD;
+    User.findOne({ where: { username: username }
 
-    const user = {
-      name: 'Kyle',
-      role: 'ADMIN',
-      favColor: 'BLUE'
-    };
+    }).then ((data) => {
+        if(data.dataValues.username !== username) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if(data.dataValues.password !== password) {
+          return done(null, false, { message: 'Incorrect password.'});
+        }
 
-    if(username === USERNAME && password === PASSWORD) {
-      return done(null, user);
-    }
-    return done(null, false);
+        return done(null, data);
+      });
   }
 ));
 
+//assign an id/cookie to browser for use after initial authentication
 passport.serializeUser((user, done) => {
   return done(null, user);
 });
-
+//remove the id/cookie from browser
 passport.deserializeUser((user, done) => {
   return done(null, user);
 });
 
+//midware
 const isAuthenticated = (req, res, next) => {
   if(!req.isAuthenticated()) {
     return res.redirect('/login');
@@ -83,98 +82,18 @@ app.get('/login', (req, res) => {
   res.render('./authTemplates/login');
 });
 
-app.get('/secret', isAuthenticated, (req, res) => {
-  console.log('req.user', req.user);
-  res.render('./authTemplates/secret', { role: req.user.name });
-});
-
 app.get('/logout', (req, res)=> {
   req.logout(); //clears cookies
   res.redirect('/login');
 });
 
+//redirect upon authentication
 app.post('/login', passport.authenticate('local', {
-    successRedirect: '/secret',
+    successRedirect: '/gallery',
     failureRedirect: '/login',
 }));
 
 /*=====  End of Section comment block  ======*/
-
-/**========================================
- *=========================================
- * START AUTH STUFF
- *========================================
- *========================================
- */
-/*app.set('views', 'views');
-app.set('view engine', 'jade');
-
-app.use(bodyParser.urlencoded({extended: true}));
-
-//Set session before initializing passport
-app.use(session({
-  secret: CONFIG.SECRET,
-  saveUninitialized: true}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(new LocalStrategy((username, password, done) => {
-  const CREDENTIALS = CONFIG.CREDENTIALS;
-  const USERNAME = CREDENTIALS.USERNAME;
-  const PASSWORD = CREDENTIALS.PASSWORD;
-
-  const user = {
-    name: 'Andy',
-    role: 'ADMIN',
-    favColor: 'BLUE'
-  };
-
-  if(username === USERNAME && password === PASSWORD){
-   return done(null, user);
-  }
-  return done(null, false);
-}));
-
-passport.serializeUser((user, done) => {
-  return done(null, user);
-});
-
-passport.deserializeUser((user, done) => {
-  return done(null, user);
-});
-
-//Middleware that will protect SECRET page
-const isAuthenticated = (req, res, next) => {
- if(!req.isAuthenticated()) {
-  return res.redirect('/login');
- }
- return next();
-};
-
-app.get('/login', (req, res) => {
-  res.render('login');
-});
-
-app.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect('/login');
-});
-
-app.get('/secret', isAuthenticated, (req, res) => {
-  res.render('secret', {role: req.user.name});
-});
-
-app.post('/login',
-    passport.authenticate('local', { successRedirect: '/secret',
-    failureRedirect: '/login'}));*/
-
-/**========================================
- *=========================================
- * END AUTH STUFF
- *========================================
- *========================================
- */
 
 const server = app.listen(3000, () => {
   db.sequelize.sync();
